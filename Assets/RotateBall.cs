97794ef601714eefc8c135d2e2f0057a6246b875
Yetian <yetian@Yetians-iMac.honiden.nii.ac.jp>
2016-11-27 19:27:36 +0900

    First Commit
    Used two methods to implement viewing 360 image

---
+++ Assets/RotateBall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBall : MonoBehaviour {
+
+	// Use this for initialization
+	public float smooth = 2.0F;
+	public float tiltAngle = 30.0F;
+	void Update() {
+		transform.RotateAround(transform.position, transform.up, Time.deltaTime * 20f);
+	}
+}
